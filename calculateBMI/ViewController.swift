000a3d20209423b8a4a4b0cb15430eb1b9b2d0a0
hiromichikoeda <empty969@gmail.com>
2020-01-29 00:04:53 +0900

    Initial Commit

---
+++ calculateBMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculateBMI
+//
+//  Created by Hiromichi Koeda on 2020/01/29.
+//  Copyright © 2020 Hiromichi Koeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
