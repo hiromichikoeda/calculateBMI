7b8aca0cce8d0f4399d2e211fd234afe81c19e6d
hiromichikoeda <empty969@gmail.com>
2020-01-29 19:54:19 +0900

    BMI計算機の実装

--- calculateBMI/ViewController.swift
+++ calculateBMI/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var weight: UITextField!
+    
+    @IBOutlet weak var bmiText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.height.keyboardType = UIKeyboardType.decimalPad
+        self.weight.keyboardType = UIKeyboardType.decimalPad
+
+
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        let dheight = Double(height.text!)//！はオプショナル型
+        let dweight = Double(weight.text!)
+        let dheight2 = dheight! * dheight!//nilの可能性があると！が必要？
+        let bmi = dweight! / dheight2
+        let bmiFloor = floor(bmi*100)/100// 3.14（小数第3位で切り捨て）
+        print("bmiFloor")
+        bmiText.text = "あなたのBMIは" + String(bmiFloor) + "です。"
+    }
+    
 }
 
